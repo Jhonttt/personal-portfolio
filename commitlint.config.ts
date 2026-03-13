0ba35cd6fd622ed3dfd08f01b26dd1162105f279
Juan Atahona <juanatahonadev@gmail.com>
2026-03-13 12:11:05 +0100

    chore: add the prefix design to the conventional commits

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,4 +2,24 @@ import type { UserConfig } from '@commitlint/types'
 
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'design',
+      ],
+    ],
+  },
 } satisfies UserConfig
