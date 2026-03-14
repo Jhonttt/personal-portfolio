38a3a7247c821a234f4190e134f2cb27f6b84339
Jhonttt <juanatahonadev@gmail.com>
2026-03-15 00:49:31 +0100

    feat: create AppSocial

--- app/components/layout/AppSocial.vue
+++ app/components/layout/AppSocial.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+  import { usePortfolioStore } from '#imports'
+
+  const socials = usePortfolioStore().footer.socials
+</script>
+
 <template>
-  <div />
+  <div>
+    <ul class="flex items-center gap-6">
+      <li v-for="social in socials" :key="social.label">
+        <a :href="social.href" class="transition">
+          <svg class="hover:fill-accent transition fill-white size:5 md:size-5 sm:size-5 xs:size-5">
+            <use :href="'/sprite.svg#' + social.icon" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
