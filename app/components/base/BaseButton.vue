a539e3dfd7be90b8b63f7dc8880e3fe75d6d690d
Jhonttt <juanatahonadev@gmail.com>
2026-03-15 22:55:39 +0100

    feat: create reusable BaseButton

---
+++ app/components/base/BaseButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  interface Props {
+    link: string
+    class?: string
+  }
+
+  const props = defineProps<Props>()
+</script>
+
+<template>
+  <a
+    href="{{ props.link }}"
+    :class="[
+      'border-2 rounded-md py-1.5 px-3.5 text-xs font-bold hover:text-text-primary transition',
+      props.class,
+    ]"
+  >
+    <slot>Default button</slot>
+  </a>
+</template>
