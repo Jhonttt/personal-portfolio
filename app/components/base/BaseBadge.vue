80f8cfff38ed2df7bc6fcfb81b0918057568f27c
Jhonttt <juanatahonadev@gmail.com>
2026-03-15 23:39:43 +0100

    feat: create reusable BaseBadge

---
+++ app/components/base/BaseBadge.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  interface Props {
+    labels: string[]
+  }
+
+  const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex items-center gap-1">
+    <span
+      v-for="(label, index) in props.labels"
+      :key="label"
+      class="text-accent text-xs uppercase font-bold tracking-wider"
+    >
+      {{ label }}<template v-if="index < props.labels.length - 1"> • </template>
+    </span>
+  </div>
+</template>
