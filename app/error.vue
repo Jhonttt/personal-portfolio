fac9d9c4b4ec80a46430c60b307a63d132cc3e35
Juan Atahona <juanatahonadev@gmail.com>
2026-03-16 10:14:47 +0100

    feat: create the error.vue with the errors status

--- app/error.vue
+++ app/error.vue
@@ -9,22 +9,42 @@
 </script>
 
 <template>
-  <div class="error-page">
+  <div
+    class="error-page min-h-screen flex flex-col items-center justify-center gap-3 text-center border"
+  >
     <template v-if="error.status === 404">
-      <h1>Page not found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <div>
+        <h1>
+          <span class="text-xl text-accent">{{ error.status }}</span> Page not found
+        </h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
     </template>
 
     <template v-else-if="(error.status ?? 0) >= 500">
-      <h1>Server error</h1>
-      <p>Something went wrong. Please try again later.</p>
+      <div>
+        <h1>
+          <span class="text-xl text-accent">{{ error.status }}</span> Server error
+        </h1>
+        <p>Something went wrong. Please try again later.</p>
+      </div>
     </template>
 
     <template v-else>
-      <h1>Error {{ error.status }}</h1>
-      <p>{{ error.message }}</p>
+      <div>
+        <h1>
+          <span class="text-xl text-accent">{{ error.status }}</span> Error
+        </h1>
+        <p>{{ error.message }}</p>
+      </div>
     </template>
 
-    <button @click="handleError">Go home</button>
+    <div @click="handleError">
+      <BaseButton
+        link="/"
+        class="border-2 rounded-md py-1.5 px-3.5 text-xs font-bold hover:text-text-primary hover:bg-accent hover:border-accent transition"
+        >Go home</BaseButton
+      >
+    </div>
   </div>
 </template>
