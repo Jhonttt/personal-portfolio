59ac43cf33eae3df2a3c625984ba6cc887de8be8
Juan Atahona <juanatahonadev@gmail.com>
2026-03-17 12:14:47 +0100

    chore: make it more accessible

--- app/components/base/BaseProject.vue
+++ app/components/base/BaseProject.vue
@@ -11,25 +11,28 @@
 </script>
 
 <template>
-  <div
-    class="flex flex-col mt-5 group cursor-pointer"
-    :aria-labelledby="`card-${props.title}`"
-    role="group"
+  <a
+    class="flex flex-col mt-5 group"
+    :aria-labelledby="`project-${props.title}`"
+    :href="url"
+    target="_blank"
+    rel="noopener noreferrer"
   >
     <div class="rounded-3xl overflow-hidden border border-dark-border mb-5 h-72">
       <img
         :src="props.image"
-        :alt="props.title"
+        :alt="`Captura de pantalla del proyecto ${props.title}`"
         class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
       />
     </div>
     <div class="flex items-center justify-between">
       <div class="flex flex-col gap-1">
         <BaseBadge :labels="props.tags" />
-        <h3 class="text-xl font-bold">{{ props.title }}</h3>
+        <h3 :id="`project-${props.title}`" class="text-xl font-bold">{{ props.title }}</h3>
       </div>
       <div
         class="size-12 rounded-full border border-dark-border flex items-center justify-center group-hover:bg-accent group-hover:border-accent transition-colors fill-text-primary"
+        aria-hidden="true"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <path d="M5.4 20L4 18.6L15.6 7H9V5h10v10h-2V8.4z" />
@@ -39,5 +42,5 @@
     <p class="text-text-muted text-pretty font-bold leading-relaxed text-sm">
       {{ props.description }}
     </p>
-  </div>
+  </a>
 </template>
--- app/components/sections/ProjectsSection.vue
+++ app/components/sections/ProjectsSection.vue
@@ -4,18 +4,33 @@
 </script>
 
 <template>
-  <section id="hero" aria-labelledby="hero-heading" class="py-20 px-6 max-w-5xl mx-auto w-full">
+  <section
+    id="projects"
+    aria-labelledby="project-heading"
+    class="py-20 px-6 max-w-5xl mx-auto w-full"
+  >
     <div class="flex flex-col gap-2" role="region" aria-labelledby="hero-heading">
-      <h2 id="about-heading" class="text-2xl font-black">
+      <h2 id="projects-heading" class="text-2xl font-black">
         {{ projects.title }}
       </h2>
       <div class="flex justify-between">
         <p class="text-text-muted text-pretty font-bold leading-relaxed text-sm">
           {{ projects.description }}
         </p>
-        <a :href="projects.cta.href" class="flex items-center gap-1 transition group">
+        <a
+          :href="projects.cta.href"
+          class="flex items-center gap-1 transition group"
+          aria-label="See all the projects"
+        >
           <span class="text-sm font-bold">{{ projects.cta.label }}</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            focusable="false"
+          >
             <path
               fill="currentColor"
               d="M16.175 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.175l-4.9-4.9q-.3-.3-.288-.7t.313-.7q.3-.275.7-.288t.7.288l6.6 6.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-6.6 6.6q-.275.275-.687.275T11.3 19.3q-.3-.3-.3-.712t.3-.713z"
@@ -25,23 +40,16 @@
         </a>
       </div>
     </div>
-    <div
-      v-if="projects.items && projects.items.length > 0"
-      class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10"
+
+    <ul
+      v-if="projects.items?.length"
+      class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 list-none p-0"
+      aria-label="List of projects"
     >
-      <BaseProject
-        v-for="project in projects.items"
-        :id="project.id"
-        :key="project.id"
-        :title="project.title"
-        :description="project.description"
-        :image="project.image"
-        :tags="project.tags"
-        :url="project.url"
-      />
-    </div>
-    <div v-else class="text-center mt-3">
-      <p class="text-2xl">There aren't projects</p>
-    </div>
+      <li v-for="project in projects.items" :key="project.id">
+        <BaseProject v-bind="project" />
+      </li>
+    </ul>
+    <p v-else class="text-center text-2xl mt-3" role="status">There aren't projects</p>
   </section>
 </template>
