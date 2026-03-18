60029b05e46c5116d260acab028a9faedd23c2fe
Juan Atahona <juanatahonadev@gmail.com>
2026-03-18 17:28:49 +0100

    feat: improve the accessibility in the base project component

--- app/components/base/BaseProject.vue
+++ app/components/base/BaseProject.vue
@@ -14,6 +14,7 @@
   <a
     class="flex flex-col mt-5 group"
     :aria-labelledby="`project-${props.title}`"
+    :aria-describedby="`project-desc-${props.title}`"
     :href="url"
     target="_blank"
     rel="noopener noreferrer"
@@ -28,18 +29,31 @@
     <div class="flex items-center justify-between">
       <div class="flex flex-col gap-1">
         <BaseBadge :labels="props.tags" />
-        <h3 :id="`project-${props.title}`" class="text-xl font-bold">{{ props.title }}</h3>
+        <h3 :id="`project-${props.title}`" class="text-xl font-bold">
+          {{ props.title }}
+          <span class="sr-only">(opens in new tab)</span>
+        </h3>
       </div>
       <div
         class="size-12 rounded-full border border-dark-border flex items-center justify-center group-hover:bg-accent group-hover:border-accent transition-colors fill-text-primary"
         aria-hidden="true"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
           <path d="M5.4 20L4 18.6L15.6 7H9V5h10v10h-2V8.4z" />
         </svg>
       </div>
     </div>
-    <p class="text-text-muted text-pretty font-bold leading-relaxed text-sm">
+    <p
+      :id="`project-desc-${props.title}`"
+      class="text-text-muted text-pretty font-bold leading-relaxed text-sm"
+    >
       {{ props.description }}
     </p>
   </a>
