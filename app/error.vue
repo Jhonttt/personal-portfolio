fea4dce38a44b0bcc314ba609b2fa2e90f36ecf6
Juan Atahona <juanatahonadev@gmail.com>
2026-03-18 16:23:54 +0100

    feat: improve the accessibility in the error page

--- app/error.vue
+++ app/error.vue
@@ -9,40 +9,41 @@
 </script>
 
 <template>
-  <div class="error-page min-h-screen flex flex-col items-center justify-center gap-3 text-center">
+  <main class="error-page min-h-screen flex flex-col items-center justify-center gap-3 text-center">
     <template v-if="error.status === 404">
-      <div>
-        <h1>
-          <span class="text-xl text-accent">{{ error.status }}</span> Page not found
+      <div role="alert">
+        <h1 aria-label="Error 404: Page not found">
+          <span aria-hidden="true" class="text-xl text-accent">{{ error.status }}</span> Page not
+          found
         </h1>
         <p>The page you are looking for does not exist.</p>
       </div>
     </template>
 
     <template v-else-if="(error.status ?? 0) >= 500">
-      <div>
-        <h1>
-          <span class="text-xl text-accent">{{ error.status }}</span> Server error
+      <div role="alert">
+        <h1 :aria-label="`Error ${error.status}: Server Error`">
+          <span aria-hidden="true" class="text-xl text-accent">{{ error.status }}</span> Server
+          error
         </h1>
         <p>Something went wrong. Please try again later.</p>
       </div>
     </template>
 
     <template v-else>
-      <div>
-        <h1>
-          <span class="text-xl text-accent">{{ error.status }}</span> Error
+      <div role="alert">
+        <h1 :aria-label="`Error ${error.status}`">
+          <span aria-hidden="true" class="text-xl text-accent">{{ error.status }}</span> Error
         </h1>
         <p>{{ error.message }}</p>
       </div>
     </template>
 
-    <div @click="handleError">
-      <BaseButton
-        link="/"
-        class="border-2 rounded-md py-1.5 px-3.5 text-xs font-bold hover:text-text-primary hover:bg-accent hover:border-accent transition"
-        >Go home</BaseButton
-      >
-    </div>
-  </div>
+    <BaseButton
+      link="/"
+      class="border-2 rounded-md py-1.5 px-3.5 text-xs font-bold hover:text-text-primary hover:bg-accent hover:border-accent transition"
+      @click="handleError"
+      >Go home</BaseButton
+    >
+  </main>
 </template>
