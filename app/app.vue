80eb7df88d748ef94a7f37e3353bb107bbffc005
Juan Atahona <juanatahonadev@gmail.com>
2026-03-18 16:27:52 +0100

    feat: improve the accessibility in the app page

--- app/app.vue
+++ app/app.vue
@@ -4,10 +4,17 @@
 </script>
 
 <template>
-  <div v-if="store.loadError" class="min-h-screen flex items-center justify-center">
+  <div
+    v-if="store.loadError"
+    role="alert"
+    aria-live="assertive"
+    class="min-h-screen flex items-center justify-center"
+  >
     <p class="text-secondary">{{ store.loadError }}</p>
   </div>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div :aria-busy="!store.isLoaded ? 'true' : 'false'" aria-live="polite">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
