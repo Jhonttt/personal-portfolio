3ba722013dc9c0884011223f2a33430b10c4a6f8
Juan Atahona <juanatahonadev@gmail.com>
2026-03-18 17:15:35 +0100

    feat: remove the redundant aria-label

--- app/components/layout/AppHeader.vue
+++ app/components/layout/AppHeader.vue
@@ -5,10 +5,10 @@
 </script>
 
 <template>
-  <header class="border-b border-dark-border" aria-label="header">
+  <header class="border-b border-dark-border">
     <div class="py-4 flex items-center px-6 max-w-7xl mx-auto w-full relative">
       <a
-        href="#"
+        href="/"
         class="uppercase font-black flex items-center gap-2 text-text-primary shrink"
         :aria-label="`${navigation.logo.title} - go home`"
       >
